6ad3eaddcb08725be2e4980e9c0d29fe4ad05240
NickBwalley <nickbwalley@gmail.com>
2022-12-19 10:47:47 +0300

    last commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const express = require('express');
-const dotenv = require('dotenv').config();
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -9,11 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Set static folder 
-app.use(express.static(path.join(__dirname, 'public')))
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/openai', require('./routes/openaiRoutes'))
+app.use("/openai", require("./routes/openaiRoutes"));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
-
-
